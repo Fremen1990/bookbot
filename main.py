89cf5146f483a60a5bb27bf3f103893965c84545
Fremen1990 <fremen1990@gmail.com>
2024-03-25 19:55:59 +0100

    Add functionality to generate report

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 path_to_file="books/frankenstein.txt"
 
-
 def count_letters(s):
-    # Initialize an empty dictionary
     letter_count = {}
 
-    # Iterate over each character in the string
     for char in s:
-        # Check if the character is a letter
         if char.isalpha():
-            # Convert the letter to lowercase to ensure case-insensitivity
             char = char.lower()
 
-            # If the letter is already in the dictionary, increment its count
             if char in letter_count:
                 letter_count[char] += 1
-            # If the letter is not in the dictionary, add it with a count of 1
             else:
                 letter_count[char] = 1
 
@@ -25,12 +18,18 @@ def count_letters(s):
 with open(path_to_file) as bookText:
     file_contents=bookText.read()
 
-    # print(file_contents)
-
-    # words = file_contents.split()
-    # print(len(words))
-
-    # // create dictionary which will count number of each letter in the provided string
+    words = file_contents.split()
 
     letter_counts = count_letters(file_contents)
-    print(letter_counts)
+
+    # print(letter_counts.items())
+
+    letter_counts_list_sorted = sorted(letter_counts.items())
+
+print("--- Begin report of books/frankenstein.txt ---")
+print(f'{len(words)} words found in the document\n')
+
+for letter in letter_counts_list_sorted:
+    print(f'The "{letter[0]}" character was found {letter[1]} times')
+
+print('--- End report ---')
